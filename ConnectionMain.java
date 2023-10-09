6a196e0601bd5dd0359a7977d64b2822128ae1ee
David <1911663h@umich.mx>
2023-10-09 08:49:38 -0600

    cambios

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,13 +1,17 @@
 import java.util.List;
+import com.google.gson.Gson;
 
-import
 public class ConnectionMain {
     public static void main(String[] args){
         Conexion con = new Conexion();
-        String txtJson=con.sendGet();
-        System.out.println(txtJson);
+        //System.out.ptintln(txtJson);
+        String txtJson = con.sendGet();
         Gson gson = new Gson();
-        List<Userexample
-        con.sendGet();
+        List<UserExample> lista = gson.fromJson(txtJson, List.class);
+        System.out.println("numero de registros: " + lista.size());
+        
+      for(UserExample user : lista){
+        System.out.println(user.getId());
+      }
     }
 }
--- UserExample.java
+++ UserExample.java
@@ -1,6 +1,41 @@
-public class Userexample {
+public class UserExample {
     private int userId;
-    private 
-    private
-    private
+    private int id;
+    private String title;
+    private String body;
+    
+    
+    public int getUserId(){
+        return this.userId;
+
+    }
+     public int getId(){
+        return this.id;
+     
+    }
+     public String getTitle(){
+        return this.title;
+        
+    }
+     public String getBody(){
+        return this.body;
+        
+    }
+
+    public void setUserId(int userId){
+        this.userId = userId;
+    }
+     public void setId(int Id){
+        this.id = Id;
+    }
+     public void setTitle(String title){
+        this.title = title;
+    }
+     public void setBody(String Body){
+        this.body = Body;
+    
+
+
+
+    }
 }
